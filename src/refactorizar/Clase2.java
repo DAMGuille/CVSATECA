eda49d2849bdb7a1c88333a04659bb8857238307
guill <guill@DESKTOP-NRLDOL1>
2025-04-08 16:32:12 +0200

    en la rama PRODUCCION se crea Clase 2 y tag BETA

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactorizar;
+
+/**
+ *
+ * @author guill
+ */
+public class Clase2 {
+    int ejercicio = 7;
+    String rama = "Esto esta creado en la rama PRODUCCION";
+}
